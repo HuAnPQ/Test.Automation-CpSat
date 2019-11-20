54eeb70b176be3522c460c8ab0563359005a3d5f
Hugo Ponce <hugoponcequiroz@gmail.com>
2019-11-20 17:55:04 +0000

    JsToast with POM varios ejercicios.

---
+++ Test.Automation/JsToastTest.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
+using Test.Automation.PageModels;
+
+namespace Test.Automation
+{
+    [TestClass]
+    public class JsToastTest
+    {
+        private RemoteWebDriver driver;
+
+        [TestInitialize]
+        public void IniciarDriver()
+        {
+            this.driver = new ChromeDriver("Drivers/");
+        }
+
+        [TestMethod]
+        public void TestMethod_ToastWithPageObjectModel()
+        {
+            JsToastrPOM jsToastPOM = new JsToastrPOM(this.driver);
+
+            string titulo = "Titulo del Contenedor";
+            string message = "Hola Hugo";
+
+            jsToastPOM.WriteOnTitle(titulo);
+            jsToastPOM.ClickButtonShowToast();
+            Thread.Sleep(5000);
+            Assert.AreEqual(titulo, jsToastPOM.GetToastTitleText());
+
+            jsToastPOM.WriteOnMessage("Hola Hugo");
+            jsToastPOM.ClickButtonShowToast();
+            Thread.Sleep(5000);
+            Assert.IsTrue(jsToastPOM.GetToastMessageText().Contains(message));
+
+            jsToastPOM.ClickButtonClearToasts();
+            jsToastPOM.ClickCheckOnCloseButton();
+            jsToastPOM.ClickButtonShowToast();
+            Thread.Sleep(5000);
+            Assert.IsTrue(jsToastPOM.GetButtonToastClose() != null);
+        }
+
+        [TestMethod]
+        public void TestMethod_EjercicioShowsAlert()
+        {
+            JsToastrPOM jsToastPOM = new JsToastrPOM(this.driver);
+
+            string message = "Hola Hugo";
+            string alertText = "You can perform some custom action after a toast goes away";
+
+            jsToastPOM.WriteOnMessage(message);
+            jsToastPOM.ClickCheckOnAddBehavior();
+            jsToastPOM.ClickButtonShowToast();
+            Thread.Sleep(2000);
+            Assert.IsTrue(jsToastPOM.GetToastMessageText().Contains(message));
+            jsToastPOM.ClickOnToast();
+            Thread.Sleep(2000);
+            Assert.AreEqual(alertText, jsToastPOM.GetAlertText());
+            jsToastPOM.ClickOkOnAlert();
+        }
+
+        [TestMethod]
+        //http://agilemanifesto.org/iso/es/manifesto.html
+        public void TestMethod_EjercicioTypeToast()
+        {
+            JsToastrPOM jsToastPOM = new JsToastrPOM(this.driver);
+            string message = "Hola Hugo";
+
+            jsToastPOM.WriteOnMessage(message);
+            jsToastPOM.ClickOnErrorType();
+            jsToastPOM.ClickButtonShowToast();
+            Thread.Sleep(2000);
+            Assert.IsTrue(jsToastPOM.GetTypeToast("error") != null);
+        }
+
+
+        [TestCleanup]
+        public void FinalizarDriver()
+        {
+            this.driver.Close();
+        }
+    }
+}
---
+++ Test.Automation/PageModels/JsToastrPOM.cs
@@ -0,0 +1,171 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test.Automation.PageModels
+{
+    public class JsToastrPOM
+    {
+        private IWebDriver _driver;
+        private string title;
+        private static readonly string TITLE_ID = "title";
+        private static readonly string MESSAGE_ID = "message";
+        private static readonly string CHECK_CLOSEBUTTON_ID = "closeButton";
+        private static readonly string CHECK_ADDBEHAVIOR_ID = "addBehaviorOnToastClick";
+        private static readonly string CHECK_DEBUG_ID = "debugInfo";
+        private static readonly string CHECK_PROGRESSBAR_ID = "progressBar";
+        private static readonly string CHECK_PREVENTDUPLICATES_ID = "preventDuplicates";
+        private static readonly string CHECK_ADDCLEAR_ID = "addClear";
+        private static readonly string CHECK_NEWESTONTOP_ID = "newestOnTop";
+        private static readonly string BUTTON_SHOWTOAST_ID = "showtoast";
+        private static readonly string BUTTON_CLEARTOASTS_ID = "cleartoasts";
+        private static readonly string BUTTON_CLEARLASTTOAST_ID = "clearlasttoast";
+        private static readonly string TYPEGROUP_ID = "toastTypeGroup";
+        private static readonly string SUCCESSTYPE_XPATH = "div/label[2]/input";
+        private static readonly string INFOTYPE_XPATH = "div/label[3]/input";
+        private static readonly string WARNINGTYPE_XPATH = "div/label[4]/input";
+        private static readonly string ERRORTYPE_XPATH = "div/label[5]/input";
+
+        private static readonly string TOASTCONTAINER_ID = "toast-container";
+        private static readonly string TITLE_CSSSELECTOR = ".toast-title";
+        private static readonly string MESSAGE_CSSSELECTOR = ".toast-message";
+        private static readonly string CLOSE_CSSSELECTOR = ".toast-close-button";
+
+
+        public JsToastrPOM(IWebDriver driver)
+        {
+            this._driver = driver;
+            this._driver.Navigate().GoToUrl(@"https://codeseven.github.io/toastr/demo.html");
+        }
+
+        public string GetToastMessageText()
+        {
+            return this._driver.FindElement(By.Id(TOASTCONTAINER_ID))
+                .FindElement(By.CssSelector(MESSAGE_CSSSELECTOR)).Text;
+        }
+
+        public string GetToastTitleText()
+        {
+            return this._driver.FindElement(By.Id(TOASTCONTAINER_ID))
+                .FindElement(By.CssSelector(TITLE_CSSSELECTOR)).Text;
+        }
+
+        public object GetButtonToastClose()
+        {
+            return this._driver.FindElement(By.Id(TOASTCONTAINER_ID))
+                .FindElement(By.CssSelector(CLOSE_CSSSELECTOR));
+        }
+
+        public object GetTypeToast(string type)
+        {
+            return this._driver.FindElement(By.Id(TOASTCONTAINER_ID))
+                .FindElement(By.CssSelector(".toast-" + type));
+        }
+
+        public string GetAlertText()
+        {
+            return this._driver.SwitchTo().Alert().Text;
+        }
+
+        public void WriteOnTitle(string title)
+        {
+            this._driver.FindElement(By.Id(TITLE_ID)).SendKeys(title);
+        }
+
+        public void WriteOnMessage(string message)
+        {
+            this._driver.FindElement(By.Id(MESSAGE_ID)).SendKeys(message);
+        }
+
+        private void ClickOnId(string id)
+        {
+            this._driver.FindElement(By.Id(id)).Click();
+        }
+
+        public void ClickCheckOnCloseButton()
+        {
+            this.ClickOnId(CHECK_CLOSEBUTTON_ID);
+        }
+
+        public void ClickCheckOnAddBehavior()
+        {
+            this.ClickOnId(CHECK_ADDBEHAVIOR_ID);
+        }
+        public void ClickCheckOnDebug()
+        {
+            this.ClickOnId(CHECK_DEBUG_ID);
+        }
+        public void ClickCheckOnProgressBar()
+        {
+            this.ClickOnId(CHECK_PROGRESSBAR_ID);
+        }
+
+        public void ClickCheckOnPreventDuplicates()
+        {
+            this.ClickOnId(CHECK_PREVENTDUPLICATES_ID);
+        }
+        public void ClickCheckOnAddClear()
+        {
+            this.ClickOnId(CHECK_ADDCLEAR_ID);
+        }
+        public void ClickCheckOnNewstOnTop()
+        {
+            this.ClickOnId(CHECK_NEWESTONTOP_ID);
+        }
+
+        public void ClickButtonShowToast()
+        {
+            this.ClickOnId(BUTTON_SHOWTOAST_ID);
+        }
+
+        public void ClickButtonClearToasts()
+        {
+            this.ClickOnId(BUTTON_CLEARTOASTS_ID);
+        }
+
+        public void ClickButtonClearLastToast()
+        {
+            this.ClickOnId(BUTTON_CLEARLASTTOAST_ID);
+        }
+
+        public void ClickOnToast()
+        {
+            this.ClickOnId(TOASTCONTAINER_ID);
+        }
+
+        public void ClickOkOnAlert()
+        {
+            this._driver.SwitchTo().Alert().Accept();
+        }
+
+        public void ClickOnTypeByXpath(string xpath)
+        {
+            this._driver.FindElement(By.Id(TYPEGROUP_ID))
+                .FindElement(By.XPath(xpath)).Click();
+        }
+
+        public void ClickOnSuccessType()
+        {
+            this.ClickOnTypeByXpath(SUCCESSTYPE_XPATH);
+        }
+
+        public void ClickOnInfoType()
+        {
+            this.ClickOnTypeByXpath(INFOTYPE_XPATH);
+        }
+
+        public void ClickOnWarningType()
+        {
+            this.ClickOnTypeByXpath(WARNINGTYPE_XPATH);
+        }
+
+        public void ClickOnErrorType()
+        {
+            this.ClickOnTypeByXpath(ERRORTYPE_XPATH);
+        }
+
+    }
+}
