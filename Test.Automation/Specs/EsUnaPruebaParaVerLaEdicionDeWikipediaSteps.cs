22cdc11d91ad82a953d45d1bc3fe672e2c41fe7b
Hugo Ponce <hugoponcequiroz@gmail.com>
2019-11-21 17:08:48 +0000

    Add SpecFlow Wikipedia.

---
+++ Test.Automation/Specs/EsUnaPruebaParaVerLaEdicionDeWikipediaSteps.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
+using System;
+using TechTalk.SpecFlow;
+
+namespace Test.Automation.Specs
+{
+    [Binding]
+    public class EsUnaPruebaParaVerLaEdicionDeWikipediaSteps
+    {
+        private RemoteWebDriver driver;
+
+        [Given(@"Estoy en la pagina de Wikipedia Produbanco")]
+        public void GivenEstoyEnLaPaginaDeWikipediaProdubanco()
+        {
+            this.driver = new ChromeDriver(@"C:\Users\Administrador\source\repos\Test.Automation-CpSat\Test.Automation\Drivers\");
+            this.driver.Navigate().GoToUrl(@"https://en.wikipedia.org/wiki/Produbanco");
+        }
+        
+        [Given(@"Haga Click En Editar")]
+        public void GivenHagaClickEnEditar()
+        {
+            this.driver.FindElement(By.XPath("/html/body/div[5]/div[1]/div[3]/div[1]/ul/li[2]/a"))
+                .Click();
+        }
+        
+        [Then(@"El Titulo de la pagina de edicion debaria Incluir a la palabra ""(.*)""")]
+        public void ThenElTituloDeLaPaginaDeEdicionDebariaIncluirALaPalabra(string p0)
+        {
+            string titleText = this.driver.FindElement(By.Id("firstHeading")).Text;
+            Assert.IsTrue(titleText.Contains(p0));
+            this.driver.Close();
+            this.driver.Dispose();
+        }
+    }
+}
---
+++ Test.Automation/Specs/WIkipedia.feature
@@ -0,0 +1,7 @@
+﻿Feature: Es una prueba para ver la edicion de Wikipedia
+
+@mytag
+Scenario: Edicion en Wiki
+	Given Estoy en la pagina de Wikipedia Produbanco
+	And Haga Click En Editar
+	Then El Titulo de la pagina de edicion debaria Incluir a la palabra "Produbanco"
