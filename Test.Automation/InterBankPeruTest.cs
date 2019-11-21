c1dc11eaa3f46992a5dc4c63c6155df208ecf111
Hugo Ponce <hugoponcequiroz@gmail.com>
2019-11-21 15:21:33 +0000

    Practice InterBank with Page Factory

---
+++ Test.Automation/InterBankPeruTest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.Support.PageObjects;
+using Test.Automation.Pages;
+
+namespace Test.Automation
+{
+    [TestClass]
+    public class InterBankPeruTest
+    {
+
+        private RemoteWebDriver driver;
+
+        [TestInitialize]
+        public void IniciarDriver()
+        {
+            this.driver = new ChromeDriver("Drivers/");
+        }
+
+        [TestMethod]
+        public void LlenarFormularioParaCrearCuenta()
+        {
+            InterBankPeru interbankPage = new InterBankPeru();
+            this.driver.Navigate().GoToUrl(@"https://interbank.pe/");
+
+            PageFactory.InitElements(this.driver, interbankPage);
+
+            interbankPage.CrearCuenta.Click();
+            interbankPage.NumeroDNI.SendKeys("26260606");
+            Thread.Sleep(1000);
+            interbankPage.Celular.SendKeys("990147240");
+            Thread.Sleep(1000);
+            interbankPage.Email.SendKeys("hugoponcequiroz@gmail.com");
+            Thread.Sleep(1000);
+            interbankPage.ButtonEnviar.Click();
+            Thread.Sleep(1000);
+            Assert.IsTrue(true);
+        }
+
+        [TestCleanup]
+        public void FinalizarDriver()
+        {
+            this.driver.Close();
+        }
+    }
+}
---
+++ Test.Automation/Pages/InterBankPeru.cs
@@ -0,0 +1,30 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test.Automation.Pages
+{
+    public class InterBankPeru
+    {
+        [FindsBy(How = How.ClassName, Using = "lh-cta-sorpresas__img__gift")]
+        public IWebElement CrearCuenta { get; set; }
+
+        [FindsBy(How= How.Id, Using = "cs-txtDocumentModal")]
+        public IWebElement NumeroDNI { get; set; }
+
+
+        [FindsBy(How = How.Id, Using = "cs-txtTelfModal")]
+        public IWebElement Celular { get; set; }
+
+        [FindsBy(How=How.Id,Using = "cs-txtEmailModal")]
+        public IWebElement Email { get; set; }
+
+        [FindsBy(How=How.Id,Using = "lq-sb__button--submit")]
+        public IWebElement ButtonEnviar { get; set; }
+
+    }
+}
